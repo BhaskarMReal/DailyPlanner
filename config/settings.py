10e047592bba6187eb0377cf244c5defefccf908
Death008 <bam221@exeter.ac.uk>
2025-08-20 20:41:57 +0100

    Updated settings.py

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-iuk!dipn%o!6=*7yno!$o8c_3gp73%z5i_&@5=jzo9)*r)vb%h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '*'
+]
 
 
 # Application definition
@@ -37,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authentication',
+    'planner',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -114,7 +119,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
