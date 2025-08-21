3ddde1733f132e523fae00a853eac422d1d9f3cf
BhaskarMReal <bhaskarmad@hotmail.com>
2025-08-21 11:02:16 +0100

    Modified File Names, Added Page Icons (PNG and reference), Added Sign In URL linking

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.signup, name='default'),
     path('signup/', views.signup, name='signup'),
+    path('signin/', views.signin, name='signin')
 ]
